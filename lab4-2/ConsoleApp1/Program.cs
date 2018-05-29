9979f0205436865f31b4df0231aefd6061dd3af9
Katya <ponkrkat@gmail.com>
2018-05-29 11:11:20 +0700

    lab4-init|

---
+++ lab4-2/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //blalbalb;
+            var assembly = Assembly.GetExecutingAssembly();
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            var version = fileVersionInfo.ProductVersion;
+            Console.WriteLine(version);
+            Console.ReadLine();
+        }
+    }
+}
